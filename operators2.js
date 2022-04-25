bad77bb4470f79569f1eaa23a9d4f848d0001353
Sushant Ganeshan <sgbharat.joint@gmail.com>
2022-04-25 19:38:13 +0530

    Delete operators2.js

--- operators2.js
+++
@@ -1,8 +0,0 @@
-//script for second task - string concatenation
-var first_string = "Hello";
-var second_string = "Students !";
-var third_string = "Good Morning";
-var string_result = first_string +" "+ second_string + " " + third_string;
-document.write(string_result);
-document.getElementById("head").style.fontWeight="bold";
-document.body.style.fontFamily="verdana";
